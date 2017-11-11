27b3e39e523c910e47ff6878fc02f73e52b656e5
limed <limed@mozilla.com>
2017-11-10 16:59:29 -0800

    Add mozilla.org server aliases as well

--- nubis/puppet/sites.pp
+++ nubis/puppet/sites.pp
@@ -2,6 +2,8 @@ nubis::static { 'tpe':
   servername    => 'tpe.mozilla.org',
   serveraliases => [
     'tpe.allizom.org',
-    'cf-tpe.allizom.org"
+    'cf-tpe.allizom.org',
+    'tpe.mozilla.org',
+    'cf-tpe.mozilla.org'
   ],
 }
