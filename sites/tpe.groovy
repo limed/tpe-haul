4bf3c5211f088a3f78b4564c4648a75c35b5a385
limed <limed@mozilla.com>
2017-11-11 16:17:03 -0800

    Point to actual code repository

--- sites/tpe.groovy
+++ sites/tpe.groovy
@@ -4,14 +4,14 @@ def nubisStatic = new org.mozilla.nubis.Static()
 
 node {
    stage('Prep') {
-        checkout([$class: 'GitSCM', branches: [[name: '**']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'src/'], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: true], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/mwobensmith/ssl_compat.git']]])   
+        checkout([$class: 'GitSCM', branches: [[name: '**']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'src/'], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: true], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/elin-moco/free-fox-landing-page']]])
    nubisStatic.prepSite()
    }
-   
+
   stage ('Build') {
     nubisStatic.buildSite()
   }
-    
+
   stage('Sync') {
      nubisStatic.syncSite()
   }
